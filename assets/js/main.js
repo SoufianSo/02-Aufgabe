5217c190191661f3fefc49e44a4f2b1db4b68ae6
Soufian <soufian.souilm@gmail.com>
2023-10-17 20:42:23 +0200

    aufgabe 1-15

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,15 @@
-console.log("it works");
-console.log("%c 100", "color:red;background-color:yellow;");
-console.log(100 == 100);
-console.log("%c 3.14", "color:red; backgroundcolor:white");
-console.log(Boolean(3.14));
-let x = -15;
-console.log(Boolean(x));
-console.log("%c hello", "color:red;background-color:yellow;");
-console.log(Boolean("hello"));
-console.log("%c false", "color:red;background-color:yellow;");
-console.log(Boolean("false"));
-let a = 1;
-let b = 7;
-let c = 3.14;
-console.log(a + b + c);
-console.log((a == b) == c);
-console.log("%c 0", "color:red; background-color:yellow");
-console.log(Boolean(0));
+console.log("it works-aufgabe 1-15");
+let num = 5;
+let anothernum = 8;
+console.log(num == anothernum);
+console.log(num > anothernum);
+console.log(num < anothernum);
+console.log(num >= anothernum);
+console.log(num <= anothernum);
+console.log(num == anothernum);
+console.log(num == "5");
+console.log(num != 8);
+console.log(num != 5);
 
-console.log("%c NaN:", "color: red; background-color: yellow;");
-console.log(Boolean(NaN));
 
-console.log("%c null:", "color: red; background-color: yellow;");
-console.log(Boolean(null));
-
-console.log("%c false:", "color: red; background-color: yellow;");
-console.log(Boolean(false));
+console.log("%c"; "color:red, backgroundcolor:yellow")
