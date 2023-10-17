da11d95b8d6e7cee7202bd00ce430fe28c82fe7a
Soufian <soufian.souilm@gmail.com>
2023-10-17 15:30:57 +0200

    Intro-level-1-14y

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+console.log("it works");
+console.log("%c 100", "color:red;background-color:yellow;");
+console.log(100 == 100);
+console.log("%c 3.14", "color:red; backgroundcolor:white");
+console.log(Boolean(3.14));
+let x = -15;
+console.log(Boolean(x));
+console.log("%c hello", "color:red;background-color:yellow;");
+console.log(Boolean("hello"));
+console.log("%c false", "color:red;background-color:yellow;");
+console.log(Boolean("false"));
+let a = 1;
+let b = 7;
+let c = 3.14;
+console.log(a + b + c);
+console.log((a == b) == c);
+console.log("%c 0", "color:red; background-color:yellow");
+console.log(Boolean(0));
+
+console.log("%c NaN:", "color: red; background-color: yellow;");
+console.log(Boolean(NaN));
+
+console.log("%c null:", "color: red; background-color: yellow;");
+console.log(Boolean(null));
+
+console.log("%c false:", "color: red; background-color: yellow;");
+console.log(Boolean(false));
